7be06c604cc0e886a10dd0ca3349066000bda099
m-diana-dev <m.diana.dev@gmail.com>
2023-04-26 14:01:56 +0300

    add 2 microtask

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Header";
+import {Header} from "./site/Header";
+import {Body} from "./site/Body";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Body/>
         </div>
     );
 }
